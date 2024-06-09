9ac640edc2aa9fc75e7e686f7a51cb4ceaa213c9
mighty <mightyrajukushwaha@gmail.com>
2024-06-09 22:07:51 +0545

    "Updated index.html, script.js, and style.css files with new HTML structure, JavaScript code, and CSS styles."

--- script.js
+++ script.js
@@ -7,11 +7,28 @@ $(document).ready(function(){
 var timeline = gsap.timeline();
 timeline.to(".loading",{
     opacity : 0,
-    delay : 2.5
+    delay : 2.6
 })
 .to(".loader",{
     opacity : 0,
     duration : 1,
     ease : "Expo.easeInOut"
 })
-.to(".loader")
+.to(".loader",{
+    opacity : 0,
+    duration : 1,
+    ease : "Ease.easeInOut",
+
+})
+.to(".loader",{
+    y : "-100%",
+    duration : 1,
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    var textElement = document.getElementById('text');
+    setTimeout(function() {
+        textElement.classList.remove('hidden');
+        textElement.classList.add('visible');
+    }, 3510); // 3000 milliseconds = 3 seconds
+});
